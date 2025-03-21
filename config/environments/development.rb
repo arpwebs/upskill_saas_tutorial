f33d253a54f619af0594fe548ac6adc6debde80c
AR Pueblos <arpueblos18@gmail.com>
2025-03-21 19:18:47 +0000

    Updated Gemfile for consistency

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.hosts << "6efbac6b19674f719f397e69b652203f.vfs.cloud9.ap-southeast-2.amazonaws.com"
+
+
   # Do not eager load code on boot.
   config.eager_load = false
 
