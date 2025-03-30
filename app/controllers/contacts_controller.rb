0ebb22452b0fc2a5c28df42c3d0705e8d5160fc2
AR Pueblos <arpueblos18@gmail.com>
2025-03-30 17:38:57 +0000

    Added form validations to model

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,33 @@
 class ContactsController < ApplicationController
-    def new
-      @contact = Contact.new
-    end
+  def new
+    # Create a new empty Contact object to be used in the form
+    @contact = Contact.new
+  end
+  
+  def create
+    # Create a new Contact object with the parameters from the form
+    @contact = Contact.new(contact_params)
     
-    def create
-      @contact = Contact.new(contact_params)
-      if @contact.save
-         redirect_to new_contact_path, notice: "Message sent."
-      else
-         redirect_to new_contact_path, notice: "Error occured."
-      end
+    # Try to save the Contact to the database
+    if @contact.save
+      # Flash message for success
+      flash[:success] = "Message sent successfully! We'll get back to you soon."
+      # Create a fresh contact object for the form
+      @contact = Contact.new
+      # Render the new template again instead of redirecting
+      render 'new'
+    else
+      # If saving fails, just set the flash message
+      # The form will still have all the user's entered data
+      flash.now[:danger] = "Error occurred, message has not been sent. Please check the form fields."
+      # Using flash.now since we're not redirecting
+      render 'new'
     end
-    private
-      def contact_params
-         params.require(:contact).permit(:name, :email, :comments)
-      end
-
+  end
+  
+  private
+    # Strong parameters to prevent mass assignment vulnerability
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ActiveRecord::Base
+  # Validations - ensure that these fields are present
+  validates :name, presence: true
+  validates :email, presence: true
+  # Validate proper email format
+  validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates :comments, presence: true
 end
