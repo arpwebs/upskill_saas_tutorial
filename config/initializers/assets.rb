37da9f120a85a7b938c7ec364653b4391c821efc
AR Pueblos <arpueblos18@gmail.com>
2025-04-13 05:09:50 +0000

    Fix Heroku deployment issues

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 
 Rails.application.config.assets.precompile += %w( devmatch.css )
+Rails.application.config.assets.precompile += %w( *.svg *.eot *.woff *.ttf *.png *.jpg *.jpeg *.gif )
 
 
