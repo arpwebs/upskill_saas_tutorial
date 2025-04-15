cf48c3b1a9aca2d583ba7db215577b99c72ceb89
AR Pueblos <arpueblos18@gmail.com>
2025-04-15 13:26:47 +0000

    Added conditional logout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    sign_up: 'register'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root to: 'pages#home'
-    get 'about', to: 'pages#about'
-    resources :contacts, only: [:new, :create]
-    get 'contact-us', to: 'contacts#new', as: :contact_us
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :contacts, only: [:new, :create]
+  get 'contact-us', to: 'contacts#new', as: :contact_us
 
 end
