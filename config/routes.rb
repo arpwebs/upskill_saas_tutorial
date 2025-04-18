37a0da1b09c0c01616f87410a4971d054178b0ac
AR Pueblos <arpueblos18@gmail.com>
2025-04-18 06:47:44 +0000

    Added association for plans and users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
-    sign_up: 'register'
+    sign_up: 'signup'
   },
   controllers: {
     sessions: 'users/sessions'
